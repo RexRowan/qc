66adf471f8f0d2c40a4a4433c84084355abb8952
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2024-01-13 12:10:49 +0000

    debug

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -24,4 +24,3 @@ urlpatterns = [
     path('', include('app.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = "my_project.views.handler404"
--- my_project/my_project/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def handler404(requst, exception):
-    return HttpResponse("<h1>You need to be logged in to view the contents of each section.</h1>")
